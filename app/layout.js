388c0ba1259a4ef5db1621c4200619c584e9cd15
k-amith1610 <2110030372@klh.edu.in>
2024-07-17 18:23:58 +0530

    project setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   );
 }
