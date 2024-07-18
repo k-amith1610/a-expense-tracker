b45a0bd8b895347a6b9b61ba29337acf5fcd459b
k-amith1610 <2110030372@klh.edu.in>
2024-07-18 19:31:56 +0530

    added gsap animation in hero section

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,14 +4,17 @@ import { Button } from '@/components/ui/button'
 
 const Header = () => {
   return (
-    <div className="p-4 flex justify-between items-center shadow-sm">
-      <Image src={'/etlogo1.png'}
-        alt='logo'
-        width={140}
-        height={100}
-        className='rounded-t-md bg-blue-400'
-      />
-      <Button className="hover:bg-white border border-slate-900 border-y-2 border-x-2 hover:text-black">Get Started</Button>
+    <div className='fixed w-full'>
+      <div className="w-full p-4 flex justify-between items-center shadow-lg bg-black">
+        <Image src={'/etlogo1.png'}
+          alt='logo'
+          width={140}
+          height={100}
+          className='rounded-t-md bg-blue-400'
+        />
+        <Button className="hover:bg-white border border-slate-900 border-y-2 border-x-2 hover:text-black">Get Started</Button>
+      </div>
+      <div className="w-[90%] md:w-[98%] mx-auto border border-gray-200 "></div>
     </div>
   )
 }
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,22 +1,34 @@
+"use client"
 import React from 'react'
 import Image from "next/image"
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.to("#heading", {
+            opacity: 1,
+            ease: 'bounce.inOut',
+            stagger: 0.4,
+        })
+    }, [])
+
+
     return (
-        <section className="bg-gray-50 flex items-center flex-col">
+        <section className="overflow-hidden bg-slate-950 flex items-center flex-col">
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
                 <div className="mx-auto max-w-xl text-center">
-                    <h1 className="font-extrabold text-2xl md:text-4xl">
+                    <h1 id="heading" className="font-extrabold opacity-0 text-white text-2xl md:text-4xl">
                         Manage Your Expenses
                         <br />
                         <strong className="font-extrabold text-primary sm:block mt-4"> Take Control of Your Finances</strong>
                     </h1>
 
-                    <p className="mt-4  text-md text-gray-400">
+                    <p id="heading" className="mt-4 opacity-0 text-md text-slate-600 font-semibold">
                         Easily track your spending, set budgets, and gain insights into your financial habits. Stay on top of your finances with our intuitive and powerful expense tracker app.
                     </p>
 
-                    <div className="mt-8 flex flex-wrap justify-center gap-4">
+                    <div id="heading" className="mt-8 flex flex-wrap opacity-0 justify-center gap-4">
                         <a
                             className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-green-500 sm:w-auto"
                             href="#"
@@ -26,13 +38,15 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-            <Image 
-                src='/dashboardimg.jpg'
-                alt="dashboard image"
-                width={1000}
-                height={700}
-                className="mt-5 rounded-xl border-2"
-            />
+            <div id="heading" className="-mt-14 opacity-0 md:p-0 p-4">
+                <Image
+                    src='/dashboardimg.jpg'
+                    alt="dashboard image"
+                    width={1000}
+                    height={700}
+                    className="rounded-xl"
+                />
+            </div>
             <br /><br />
         </section>
     )
