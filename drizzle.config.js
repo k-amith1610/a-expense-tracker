de480619df907f3c4c993cc42921f02fb5a6b8c2
k-amith1610 <2110030372@klh.edu.in>
2024-07-23 17:33:04 +0530

    done few changes related to db

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://expensetracker_owner:wrvpaM5eK4kS@ep-aged-hall-a5dt5a0b.us-east-2.aws.neon.tech/expensetracker?sslmode=require',
+        url: process.env.NEXT_PUBLIC_DATABASE_URL,
     }
 };
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from "./schema";
-const sql = neon('postgresql://expensetracker_owner:wrvpaM5eK4kS@ep-aged-hall-a5dt5a0b.us-east-2.aws.neon.tech/expensetracker?sslmode=require');
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 const db = drizzle(sql, {schema});
