e00b8ee96de5ab65a7c14d979d082b3af0582534
k-amith1610 <2110030372@klh.edu.in>
2024-07-23 18:36:12 +0530

    done few changes

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import SideNav from './_components/SideNav'
 import DashboardHeader from './_components/DashboardHeader'
+import { db } from '@/utils/dbConfig'
+import { Budgets } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
+import { useRouter } from 'next/navigation'
 
 const DashboardLayout = ({ children }) => {
+
+
+    const { user } = useUser();
+    const router = useRouter();
+
+    useEffect(() => {
+        user && checkUserBudgets();
+    }, [user])
+    const checkUserBudgets = async () => {
+        const result = await db.select()
+            .from(Budgets)
+            .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+
+        console.log(result)
+        if(result.length == 0) {
+            router.replace("/dashboard/budget");
+        }
+    }
+
     return (
         <div className="">
             <div className="fixed md:w-64 hidden md:block bg-slate-950">
