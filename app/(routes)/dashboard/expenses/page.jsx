d9cac92a0d1c76391e61695d94e09c8f610b6ff5
k-amith1610 <2110030372@klh.edu.in>
2024-08-04 15:22:17 +0530

    Done with expenses page

--- app/(routes)/dashboard/expenses/page.jsx
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client"
+import { db } from '@/utils/dbConfig';
+import { Budgets, Expenses } from '@/utils/schema';
+import { useUser } from '@clerk/nextjs';
+import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
+import ExpenseListTable from './_components/ExpenseListTable';
 
 const page = () => {
+
+
+    const { user } = useUser();
+
+    const [budgetList, setBudgetList] = useState([]);
+    const [expensesList, setExpensesList] = useState([]);
+
+    useEffect(() => {
+        user && getBudgetList();
+    }, [user]);
+
+    const getBudgetList = async () => {
+        const result = await db.select({
+            ...getTableColumns(Budgets),
+            totalSpend: sql`SUM(CAST(${Expenses.amount} AS NUMERIC))`.mapWith(Number),
+            totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+        }).from(Budgets)
+            .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+            .where(eq(Budgets.createdBy, user.primaryEmailAddress?.emailAddress))
+            .groupBy(Budgets.id)
+            .orderBy(desc(Budgets.id));
+
+        setBudgetList(result);
+        getAllExpenses();
+    }
+
+    const getAllExpenses = async () => {
+        const result = await db.select({
+            id: Expenses.id,
+            name: Expenses.name,
+            amount: Expenses.amount,
+            createdAt: Expenses.createdAt
+        }).from(Budgets)
+            .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+            .where(eq(Budgets.createdBy, user.primaryEmailAddress?.emailAddress))
+            .orderBy(desc(Expenses.id));
+
+        setExpensesList(result);
+    }
+
     return (
-        <div>
-            expenses
+        <div className="p-5 md:p-10 text-white">
+            <h2 className="font-bold text-xl md:text-2xl py-3 animate-pulse">All Expenses</h2>
+            <ExpenseListTable
+                expensesList={expensesList}
+                refreshData={() => getBudgetList()}
+            />
         </div>
     )
 }
